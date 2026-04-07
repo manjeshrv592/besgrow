1b83329b39eb00b2d6ed0423e52cb5044883dfce
manjeshrv592 <manjeshrv592@gmail.com>
2026-04-07 14:10:04 +0530

    Made header position dynamic

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -15,6 +15,7 @@ import {
 import IconButton from "../ui/IconButton";
 import { CiMail } from "react-icons/ci";
 import { Button } from "../ui/button";
+import Container from "./Container";
 
 const languages = [
   { value: "en", label: "English", flag: "/img/flags/english.svg" },
@@ -130,7 +131,7 @@ const Header = ({ productsHref = "/products", locale }: HeaderProps) => {
   return (
     <>
       <header
-        className={`fixed top-0 left-0 z-50 flex w-full items-center justify-between px-4 transition-all duration-300 ease-in-out lg:grid lg:grid-cols-[1fr_max-content_1fr] ${
+        className={`fixed top-0 left-0 z-50 flex w-full items-center px-4 transition-all duration-300 ease-in-out ${
           isVisible || mobileNavOpen ? "translate-y-0" : "-translate-y-full"
         } bg-transparent`}
       >
@@ -154,87 +155,91 @@ const Header = ({ productsHref = "/products", locale }: HeaderProps) => {
             <GoChevronDown size={20} className="mt-[-26px]" />
           </span>
         </button>
-        {/* Overlay - fades in/out */}
-        <div
-          onClick={() => setMobileNavOpen(false)}
-          className={`fixed top-0 left-0 z-40 h-screen w-screen bg-black/80 transition-opacity duration-300 lg:hidden ${
-            mobileNavOpen
-              ? "pointer-events-auto opacity-100"
-              : "pointer-events-none opacity-0"
-          }`}
-        >
-          &nbsp;
-        </div>
-        {/* Nav - slides in from top */}
-        <nav
-          className={`fixed top-0 left-1/2 z-50 mx-auto w-full -translate-x-1/2 rounded-b-3xl bg-white px-8 py-4 font-semibold text-neutral-700 transition-transform duration-300 ease-in-out lg:relative lg:top-auto lg:left-auto lg:block lg:w-auto lg:translate-y-0 lg:translate-none lg:rounded-full lg:border lg:border-neutral-200 lg:bg-white lg:py-2 ${
-            mobileNavOpen
-              ? "translate-y-0"
-              : "-translate-y-full lg:translate-y-0"
-          }`}
-        >
-          <ul className="flex justify-center gap-6 lg:gap-[3vw]">
-            <li>
-              <Link
-                href="/"
-                className={linkClass("/")}
-                onClick={() => setMobileNavOpen(false)}
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                href={productsHref}
-                className={linkClass("/products")}
-                onClick={() => setMobileNavOpen(false)}
-              >
-                Products
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/distributors?region=europe"
-                className={linkClass("/distributors")}
-                onClick={() => setMobileNavOpen(false)}
-              >
-                Distributors
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/about-us"
-                className={linkClass("/about-us")}
-                onClick={() => setMobileNavOpen(false)}
-              >
-                About Us
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <div className="flex items-center justify-end gap-2 text-right">
-          <span className="lg:hidden">
-            <Button size="icon" asChild>
-              <Link href="/contact-us">
-                <CiMail strokeWidth={1} />
-              </Link>
-            </Button>
-          </span>
-          <span className="hidden -translate-x-27 lg:block">
-            <IconButton
-              href="/contact-us"
-              icon={
-                <CiMail
-                  className="translate-y-0.5 lg:translate-none lg:text-white"
-                  strokeWidth={1}
-                />
-              }
+        <Container className="flex-1">
+          {/* Overlay - fades in/out */}
+          <div
+            onClick={() => setMobileNavOpen(false)}
+            className={`fixed top-0 left-0 z-40 h-screen w-screen bg-black/80 transition-opacity duration-300 lg:hidden ${
+              mobileNavOpen
+                ? "pointer-events-auto opacity-100"
+                : "pointer-events-none opacity-0"
+            }`}
+          >
+            &nbsp;
+          </div>
+          {/* Nav - slides in from top */}
+          <div className="flex justify-end gap-10 lg:justify-start">
+            <nav
+              className={`fixed top-0 left-1/2 z-50 ml-auto w-full -translate-x-1/2 rounded-b-3xl bg-white px-8 py-4 font-semibold text-neutral-700 transition-transform duration-300 ease-in-out lg:relative lg:top-auto lg:left-auto lg:block lg:w-auto lg:translate-y-0 lg:translate-none lg:rounded-full lg:border lg:border-neutral-200 lg:bg-white lg:py-2 ${
+                mobileNavOpen
+                  ? "translate-y-0"
+                  : "-translate-y-full lg:translate-y-0"
+              }`}
             >
-              Contact Us
-            </IconButton>
-          </span>
-          <LanguageSwitcher currentLocale={locale} />
-        </div>
+              <ul className="flex justify-center gap-6 lg:gap-[3vw]">
+                <li>
+                  <Link
+                    href="/"
+                    className={linkClass("/")}
+                    onClick={() => setMobileNavOpen(false)}
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href={productsHref}
+                    className={linkClass("/products")}
+                    onClick={() => setMobileNavOpen(false)}
+                  >
+                    Products
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/distributors?region=europe"
+                    className={linkClass("/distributors")}
+                    onClick={() => setMobileNavOpen(false)}
+                  >
+                    Distributors
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/about-us"
+                    className={linkClass("/about-us")}
+                    onClick={() => setMobileNavOpen(false)}
+                  >
+                    About Us
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+            <div className="lg:flex lg:basis-[calc(30%-48px)] lg:justify-center">
+              <span className="mr-2 lg:hidden">
+                <Button size="icon" asChild>
+                  <Link href="/contact-us">
+                    <CiMail strokeWidth={1} />
+                  </Link>
+                </Button>
+              </span>
+              <span className="hidden lg:block">
+                <IconButton
+                  href="/contact-us"
+                  icon={
+                    <CiMail
+                      className="translate-y-0.5 lg:translate-none lg:text-white"
+                      strokeWidth={1}
+                    />
+                  }
+                >
+                  Contact Us
+                </IconButton>
+              </span>
+            </div>
+          </div>
+        </Container>
+        <LanguageSwitcher currentLocale={locale} />
       </header>
     </>
   );
